70efb8629c7d220ec5524569e63fe670621eeefe
Burrit0z <carsondzielinski@gmail.com>
2020-03-08 16:20:18 -0400

    Move clutter to .h file
    It’s good practice to have everything like declarations and defenitions in a separate .h file like this.

--- Tweak.h
+++ Tweak.h
@@ -1,3 +1,37 @@
 @interface SBFloatyFolderView : UIView
 @end
 
+
+// Defining global variables and methods
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+
+// Preferences keys
+NSDictionary *preferences;
+BOOL enabled;
+BOOL backgroundAlphaEnabled;
+double backgroundAlpha;
+BOOL cornerRadiusEnabled;
+double cornerRadius;
+BOOL pinchToCloseEnabled;
+BOOL customFrameEnabled;
+BOOL customCenteredFrameEnabled;
+CGFloat frameX;
+CGFloat frameY;
+CGFloat frameWidth;
+CGFloat frameHeight;
+
+static void reloadPrefs(){
+	preferences = [[NSUserDefaults standardUserDefaults]persistentDomainForName:@"com.burritoz.thomz.folded.prefs"];
+	enabled = [[preferences objectForKey:@"enabled"] boolValue];
+	backgroundAlphaEnabled = [[preferences objectForKey:@"backgroundAlphaEnabled"] boolValue];
+	backgroundAlpha = [[preferences objectForKey:@"backgroundAlpha"] doubleValue];
+	cornerRadiusEnabled = [[preferences objectForKey:@"cornerRadiusEnabled"] boolValue];
+	cornerRadius = [[preferences objectForKey:@"cornerRadius"] doubleValue];
+	pinchToCloseEnabled = [[preferences objectForKey:@"pinchToCloseEnabled"] boolValue];
+	customFrameEnabled = [[preferences objectForKey:@"customFrameEnabled"] boolValue];
+	customCenteredFrameEnabled = [[preferences objectForKey:@"customCenteredFrameEnabled"] boolValue];
+	frameX = [[preferences valueForKey:@"customFrameX"] floatValue];
+	frameY = [[preferences valueForKey:@"customFrameY"] floatValue];
+	frameWidth = [[preferences valueForKey:@"customFrameWidth"] floatValue];
+	frameHeight = [[preferences valueForKey:@"customFrameHeight"] floatValue];
+}
